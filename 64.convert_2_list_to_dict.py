4e81dc0961f1b193e732acf68a374d83172ba7ee
BHARATHV <bharathv0901@gmail.com>
2025-02-19 22:10:33 +0530

    python

--- 64.convert_2_list_to_dict.py
+++ 64.convert_2_list_to_dict.py
@@ -5,10 +5,10 @@
 name=["a","b","c","d"]
 marks=[100,99,98,98]
 
-# dictionary=zip(name,marks)
-# print(dict(dictionary))
+dictionary=zip(name,marks)
+print(dict(dictionary))
 
-#Using zip() and list comprehension
+# #Using zip() and list comprehension
 
-dictionary ={key:value for key,value in zip(name,marks)}
-print(dictionary)
+# dictionary ={key:value for key,value in zip(name,marks)}
+# print(dictionary)
